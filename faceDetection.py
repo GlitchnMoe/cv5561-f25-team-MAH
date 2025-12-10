5e9ca7bc328d7fdb27aeeb4a09a9d95c1493818e
Aaron Castle <castleaaron@icloud.com>
2025-12-10 13:07:06 -0600

    idk

--- faceDetection.py
+++ faceDetection.py
@@ -2,9 +2,9 @@ from ultralytics import YOLO
 import cv2
 # import torch
 
-face_detector = YOLO("yolov8n-face.pt") # this should proabbly be face
+face_detector = YOLO("yolov8n.pt") # this should proabbly be face
 
-gender_model = YOLO("y8n_agegender_gender20/weights/best.pt")
+gender_model = YOLO("runs_emotion/yolo_runs15/weights/best.pt")
 emotion_model = YOLO("y8n_emotion19/weights/best.pt")
 
 IMG_SIZE = 224
@@ -32,36 +32,27 @@ while True:
             face_resized = cv2.resize(face, (IMG_SIZE, IMG_SIZE))
 
             g_result = gender_model(face_resized)
-            g_probs = g_result[0].probs 
-            gender_idx = g_probs.top1
-            gender_label = g_result[0].names[gender_idx]
-            gender_conf = g_probs.top1conf
-            # g_pred = gender_model(face_resized)[0]
-            # gender_label = "Unknown"
-            # if len(g_pred.boxes) > 0:
-            #     best = g_pred.boxes[0]
-            #     g_cls = int(best.cls)
-            #     g_conf = float(best.conf)
-            #     gender_label = f"{gender_model.names[g_cls]} {g_conf:.2f}"
+
+            # The gender model is ALSO a detection model → loop through gender boxes
+            for g_r in g_result:
+                for g_box in g_r.boxes:
+                    g_cls = int(g_box.cls[0])
+                    g_conf = float(g_box.conf[0])
+
+                    # If your model has class names:
+                    gender_label = gender_model.names[g_cls]
 
             e_res = emotion_model(face_resized)
             e_probs = e_res[0].probs
             e_idx = e_probs.top1
             e_label = e_res[0].names[e_idx]
             e_conf = e_probs.top1conf
-            # e_pred = emotion_model(face_resized)[0]
-            # emotion_label = "Unknown"
-            # if len(e_pred.boxes) > 0:
-            #     best = e_pred.boxes[0]
-            #     e_cls = int(best.cls)
-            #     e_conf = float(best.conf)
-            #     emotion_label = f"{emotion_model.names[e_cls]} {e_conf:.2f}"
 
             label = f"{gender_label} | {e_label}"
 
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (255,255,255), 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255,0,0), 2)
             cv2.putText(frame, label, (x1, y1 - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0), 2)
 
     cv2.imshow("Gender + Emotion Detection", frame)
 
